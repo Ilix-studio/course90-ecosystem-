65d7b5ebe60f4531b6c7bea8dd9bb3333e56030d
Ilish jyotish <40943217+Ilix-studio@users.noreply.github.com>
2024-09-01 04:39:55 +0530

    expo firebase setup

---
+++ prototype_RN/app/(root)/finde-ride.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FindRide = () => {
+  return (
+    <View>
+      <Text>finde-ride</Text>
+    </View>
+  );
+};
+
+export default FindRide;
